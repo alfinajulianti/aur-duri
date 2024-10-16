2624608562c7e378ebee952e4fbd645a1b4dd9f7
alfinajulianti <221910918@stis.ac.id>
2024-10-17 06:05:24 +0700

    konfigurasi database

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'koto-teluk',
+	'hostname' => 'bpssungaipenuh.com',
+	'username' => 'u186259752_koto_teluk',
+	'password' => 'K0t0teluk',
+	'database' => 'u186259752_koto_teluk',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
