c7273a550e7d6fad2617ef5984aa54b01323129e
alfinajulianti <221910918@stis.ac.id>
2024-10-17 05:52:06 +0700

    ganti base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/koto-teluk/';
+$config['base_url'] = 'http://bpssungaipenuh.com/koto-teluk/';
+//$config['base_url'] = 'http://localhost/koto-teluk/koto-teluk/';
 
 /*
 |--------------------------------------------------------------------------
