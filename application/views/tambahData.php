234d3af1b3ff71f096881df8b80e23f2b6601e27
alfinajulianti <221910918@stis.ac.id>
2024-10-17 06:29:21 +0700

    hapus yg ga perlu di tambahData.php

--- application/views/tambahData.php
+++ application/views/tambahData.php
@@ -143,37 +143,6 @@
                                             <option>8. Lainnya</option>
                                         </select>
                                     </div>
-                                    <div class="col-md-6">
-                                        <label for="inputEmail5" class="form-label">Email</label>
-                                        <input type="email" class="form-control" id="inputEmail5">
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label for="inputPassword5" class="form-label">Password</label>
-                                        <input type="password" class="form-control" id="inputPassword5">
-                                    </div>
-                                    <div class="col-12">
-                                        <label for="inputAddress5" class="form-label">Address</label>
-                                        <input type="text" class="form-control" id="inputAddres5s" placeholder="1234 Main St">
-                                    </div>
-                                    <div class="col-12">
-                                        <label for="inputAddress2" class="form-label">Address 2</label>
-                                        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label for="inputCity" class="form-label">City</label>
-                                        <input type="text" class="form-control" id="inputCity">
-                                    </div>
-                                    <div class="col-md-4">
-                                        <label for="inputState" class="form-label">State</label>
-                                        <select id="inputState" class="form-select">
-                                            <option selected>Choose...</option>
-                                            <option>...</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <label for="inputZip" class="form-label">Zip</label>
-                                        <input type="text" class="form-control" id="inputZip">
-                                    </div>
                                     <div class="col-12">
                                         <div class="form-check">
                                             <input class="form-check-input" type="checkbox" id="gridCheck">
@@ -183,8 +152,7 @@
                                         </div>
                                     </div>
                                     <div class="text-center">
-                                        <button type="submit" class="btn btn-primary">Submit</button>
-                                        <button type="reset" class="btn btn-secondary">Reset</button>
+                                        <button type="submit" class="btn btn-primary">Tambah Data</button>
                                     </div>
                                 </form>
                             </div>
