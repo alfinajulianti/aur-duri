ee0a7359e926101a6b189cfc16190ac0fa50f07a
alfinajulianti <221910918@stis.ac.id>
2024-10-18 05:40:56 +0700

    menambahkan variabel data

--- infografis/application/controllers/Infografis.php
+++ infografis/application/controllers/Infografis.php
@@ -98,9 +98,9 @@ class Infografis extends CI_Controller {
 
         $data['rt'] = $this->Model_admin->ambil_kolom('*','tabel_rt','id_rt IS NOT NULL');
 
-        $this->load->view('admin/header');
+        $this->load->view('admin/header',$data);
         $this->load->view('infografis/beranda',$data);
-        $this->load->view('admin/footer');
+        $this->load->view('admin/footer',$data);
     }
 
     public function view()
@@ -162,8 +162,8 @@ class Infografis extends CI_Controller {
 
         $data['rt'] = $this->Model_admin->ambil_kolom('*','tabel_rt','id_rt IS NOT NULL');
 
-        $this->load->view('admin/header');
+        $this->load->view('admin/header',$data);
         $this->load->view('infografis/beranda',$data);
-        $this->load->view('admin/footer');
+        $this->load->view('admin/footer',$data);
     }
 }
