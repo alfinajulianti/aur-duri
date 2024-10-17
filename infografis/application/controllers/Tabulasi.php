9665efd29f6366e2bf8832865628fbf34260467f
alfinajulianti <221910918@stis.ac.id>
2024-10-18 04:58:14 +0700

    huruf besar

--- infografis/application/controllers/Tabulasi.php
+++ infografis/application/controllers/Tabulasi.php
@@ -6,16 +6,16 @@ class Tabulasi extends CI_Controller {
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->model(array('model_admin'));
+		$this->load->model(array('Model_admin'));
         $this->load->library(array('frame'));
 	}
 
 	public function index()
     {
         
-        $data['tabel_1_1_a'] = $this->model_admin->jumlah_data_where('id','tabel_penduduk','pendidikan = 1 AND kelompok_pendapatan = 1');
+        $data['tabel_1_1_a'] = $this->Model_admin->jumlah_data_where('id','tabel_penduduk','pendidikan = 1 AND kelompok_pendapatan = 1');
 
-        $data['rt'] = $this->model_admin->ambil_kolom('*','tabel_rt','id_rt IS NOT NULL');
+        $data['rt'] = $this->Model_admin->ambil_kolom('*','tabel_rt','id_rt IS NOT NULL');
 
         $this->frame->frame1('tabulasi/beranda',$data);
     }
